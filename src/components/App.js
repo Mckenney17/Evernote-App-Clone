10ca7487dc6672cd16b35fdfc8b2a32dce6ee75c
Mckenney <oluwarinolasam@gmail.com>
2021-11-08 17:38:18 +0100

    initialized parent sections

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './App.scss'
+import Editor from './Editor'
+import Notelist from './Notelist'
+import Sidebar from './Sidebar'
+
+// sidebar
+// notelist
+// editor
 
 function App() {
     return (
-        <div>
-            
+        <div className="app-wrapper">
+            <Sidebar />
+            <Notelist />
+            <Editor/>
         </div>
     )
 }
---
+++ src/components/Notelist.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Notelist() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Notelist
