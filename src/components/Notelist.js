a515d71da77783c2831b1310d2dd3095dc6fd20e
Mckenney <oluwarinolasam@gmail.com>
2021-11-09 13:48:43 +0100

    panel resizing

--- src/components/Notelist.js
+++ src/components/Notelist.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Notelist.scss'
 
 function Notelist() {
+    useEffect(() => {
+        const resize = (ev) => {
+            document.querySelector('.notelist').style.width = `${ev.clientX - document.querySelector('.sidebar').getBoundingClientRect().width}px`
+        }
+        const resizer = document.querySelector('.notelist .resizer');
+        resizer.addEventListener('mousedown', (ev) => {
+            document.addEventListener('mousemove', resize, false)
+        }, false)
+        document.addEventListener('mouseup', () => {
+            document.removeEventListener('mousemove', resize, false)
+        }, false)
+    }, [])
+
     return (
         <div className="notelist">
-            
+            <span className="resizer"></span>
         </div>
     )
 }
