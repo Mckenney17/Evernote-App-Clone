2ec7d2f54deb7d3dd16bb35ba4934ac648c562ef
Mckenney <oluwarinolasam@gmail.com>
2021-11-19 17:14:50 +0100

    text level card template

---
+++ src/components/TextLevelsCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TextLevelsCard() {
+    return (
+        <div className="text-level-card">
+            
+        </div>
+    )
+}
+
+export default TextLevelsCard
