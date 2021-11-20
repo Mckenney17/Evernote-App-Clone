e15c87e5ff534c4bb3e3b1b036c59c5461b9201d
Mckenney <oluwarinolasam@gmail.com>
2021-11-20 13:43:20 +0100

    select options card adjustments

--- src/components/FontFamiliesCard.js
+++ src/components/FontFamiliesCard.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './FontFamiliesCard.scss'
 
-function FontFamiliesCard() {
+function FontFamiliesCard({ toolsState, setToolsState }) {
     return (
         <div className="font-families-card">
             <ul>
             {[['Sans serif', 'Source Sans Pro'], ['Serif', 'Source Serif Pro'], ['Slab serif', 'Zilla Slab'], ['Monospace', 'Source Code Pro'], ['Script', 'Dancing Script'], ['Handwritten', 'Kalam']].map(([superFamily, fontFamily]) => (
-                <li key={superFamily}><button style={{ fontFamily }}>{superFamily}</button></li>
+                <li key={superFamily} className={toolsState.fontFamily === superFamily ? 'checked' : ''}>
+                    <button style={{ fontFamily }}>{superFamily}</button>
+                </li>
             ))}
             </ul>
         </div>
--- src/components/FontSizeCard.js
+++ src/components/FontSizeCard.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import './FontSizeCard.scss'
 
-function FontSizeCard() {
+function FontSizeCard({ toolsState, setToolsState }) {
     return (
         <div className="font-size-card">
             <ul>
             {[8,9,10,12,14,16,18,20,24,30,36,48,64,72,96].map((v, i) => (
-                <li key={`fz-${v}`}><button>{v}</button></li>
+                <li key={`fz-${v}`} className={toolsState.fontSize === v ? 'checked' : ''}>
+                    <button>
+                        <span className="check-mark-icon">
+                            <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M17.572 6.35a1.013 1.013 0 011.531 1.325l-8.212 9.488a1.013 1.013 0 01-1.532 0L5.497 12.7a1.012 1.012 0 111.531-1.325l3.097 3.578 7.447-8.603z" fill="currentColor"></path></svg>
+                        </span>
+                        {v}
+                    </button>
+                </li>
             ))}
             </ul>
         </div>
