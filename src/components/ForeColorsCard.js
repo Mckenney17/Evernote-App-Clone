77484bf4c6ad29ff606a841ed3274094fbf55801
Mckenney <oluwarinolasam@gmail.com>
2021-11-22 16:27:30 +0100

    tools checking fixes

--- src/components/ForeColorsCard.js
+++ src/components/ForeColorsCard.js
@@ -10,7 +10,7 @@ function ForeColorsCard({ toolsState, setToolsState, setSelectionDropTool }) {
 
     return (
         <motion.div className="fore-colors-card" ref={cardRef} animate={{ y: 10, opacity: 1, type: 'tween' }}>
-            <button className={foreColor === '#000' ? 'checked' : ''}><span></span>Auto</button>
+            <button className={!foreColor ? 'checked' : ''}><span></span>Auto</button>
             <ul>
                 {['#333', '#5a5a5a', '#8c8c8c', '#bfbfbf', '#fff', '#5724c2', '#B629D4', '#fc1233', '#fb5f2c', '#e59e25', '#18a841', '#1aa9b2', '#1885e2', '#0d2a99']
                 .map((color) => <li key={color}><button className={ foreColor === color ? 'checked' : '' } style={{ background: color }}></button></li>)}
