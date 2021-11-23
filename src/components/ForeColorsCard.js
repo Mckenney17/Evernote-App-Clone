2378723276248de57786543ec66c2f9ffcab3ff6
Mckenney <oluwarinolasam@gmail.com>
2021-11-23 02:01:19 +0100

    css fix

--- src/components/ForeColorsCard.js
+++ src/components/ForeColorsCard.js
@@ -9,7 +9,7 @@ function ForeColorsCard({ toolsState, setToolsState, setSelectionDropTool, selCo
 
     return (
         <motion.div className="fore-colors-card" ref={cardRef} animate={{ y: 10, opacity: 1, type: 'tween' }}>
-            <button className={!selColor.fore ? 'checked' : ''}><span></span>Auto</button>
+            <button className={selColor.fore === '#000' ? 'checked' : ''}><span></span>Auto</button>
             <ul>
                 {['#333', '#5a5a5a', '#8c8c8c', '#bfbfbf', '#fff', '#5724c2', '#B629D4', '#fc1233', '#fb5f2c', '#e59e25', '#18a841', '#1aa9b2', '#1885e2', '#0d2a99']
                 .map((color) => <li key={color}><button className={ selColor.fore === color ? 'checked' : '' } style={{ background: color }}></button></li>)}
