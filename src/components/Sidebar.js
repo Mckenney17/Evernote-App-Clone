06efa05910373aba211b1d23c971f7c9c1d798c0
Mckenney <oluwarinolasam@gmail.com>
2021-12-08 15:18:04 +0100

    bug fixes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -27,14 +27,14 @@ function Sidebar() {
             <motion.span className="sidebar-resizer" drag='x' onDrag={handleResizerDrag} dragMomentum={false} onDragEnd={handleDragEnd}></motion.span>
             <div className="top-section-search-newnote">
                 <Search />
-                <div className="newnote">
-                    <button onClick={() => createNewNote()} className="add-newnote">
+                <button onClick={() => createNewNote()} className="newnote">
+                    <div className="add-newnote">
                         <span className="add-icon">
                             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" fillRule="evenodd" d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zM11 9v2H9a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V9a1 1 0 10-2 0z"></path></svg>
                         </span>
-                    </button>
+                    </div>
                     <span>New Note</span>
-                </div>
+                </button>
             </div>
             <div className="menu-section">
                 <nav role="menubar">
