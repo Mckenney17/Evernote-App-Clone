dd726c6c9773721ad657c9784285b86614e0290b
Mckenney <oluwarinolasam@gmail.com>
2021-12-23 03:54:27 +0100

    fixes

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(async (req, res, next) => {
 })
 if (process.env.NODE_ENV === 'production'){    
     app.use(express.static(path.join(__dirname, 'client', 'build')))
-    app.get(' * ', (req, res) => {
+    app.use((req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     });
 }
