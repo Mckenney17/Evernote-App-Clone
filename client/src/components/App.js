f314cc4fb8937eb147098c785ab59972f483c83b
Mckenney <oluwarinolasam@gmail.com>
2021-12-25 14:58:51 +0100

    added pwd reset confirmation page route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
 import AuthCheck from './AuthCheck'
-import Error404 from './Error404'
 import Login from './Login'
-import RequestPwdReset from './RequestPwdReset'
 import Signup from './Signup'
+import RequestPwdReset from './RequestPwdReset'
+import ConfirmPwdReset from './ConfirmPwdReset'
 import VerificationRequest from './VerificationRequest'
 import VerificationReport from './VerificationReport'
+import Error404 from './Error404'
 import Spinner from './Spinner'
 
 const Home = React.lazy(() => import('./Home'))
@@ -20,6 +21,7 @@ function App() {
                     <Route path='/signup' exact component={Signup} />
                     <Route path='/verify_email' exact component={VerificationRequest} />
                     <Route path='/request_pwd_reset' exact component={RequestPwdReset} />
+                    <Route path='/confirm_pwd_reset' exact component={ConfirmPwdReset}/>
                     <Route path='/' exact component={AuthCheck} />
                     <Route path='/verify_email/:verificationToken' exact component={VerificationReport} />
                     <Route path='/user/:userId' exact component={Home} />
